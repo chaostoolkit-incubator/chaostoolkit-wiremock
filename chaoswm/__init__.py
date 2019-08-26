148f18af45ab0ccd27a9fdf54b2a3500483f5355
Sylvain Hellegouarch <sh@defuze.org>
2019-08-26 14:53:09 +0200

    Bump version: 0.1.0 → 0.1.1

--- chaoswm/__init__.py
+++ chaoswm/__init__.py
@@ -10,7 +10,7 @@ from chaoslib.types import Discovery, DiscoveredActivities
 name = "chaoswm"
 __author__ = """Marco Masetti"""
 __email__ = 'grubert65@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __all__ = ["discover", "__version__"]
 
 
